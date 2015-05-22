9b1f53ef9ee89c44cd307e2663e2f02661fb2e52
travism26 <travismartin266@gmail.com>
2015-05-22 14:46:04 -0400

    add autoload feature

---
+++ src/apiRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace battlefield;
+/**
+ * Created by PhpStorm.
+ * User: travismartin
+ * Date: 15-05-22
+ * Time: 10:04 AM
+ */
+
+class apiRequest {
+
+	public static function requestData($url, $datatype = 'default')
+	{
+		//$url = "https://api.github.com/users/" . $this->username;
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL            => $url,
+			CURLOPT_USERAGENT      => 'Codular Sample cURL Request'
+		));
+
+		// Send the request & save response to $resp
+		$resp        = curl_exec($curl);
+		$responseObj = json_decode($resp);
+		/*
+		 * use polymorphism to return the datatype we want
+		 * TBC
+		 */
+		return $responseObj;
+	}
+}
---
+++ src/user.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace battlefield;
+
+/**
+ * Created by PhpStorm.
+ * User: travismartin
+ * Date: 15-05-22
+ * Time: 12:15 PM
+ */
+
+namespace battlefield;
+
+
+class user {
+
+	public function getName()
+	{
+		return var_dump("Hello");
+	}
+}
